e3d21bacb920418baf245a6cb2db8301b137211c
JulianLivrone <julian.livrone@gmail.com>
2023-04-10 15:13:29 -0300

    Added select category option to get only quotes for a specific category

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
+const categoryText = document.getElementById("category");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
+const selectCategory = document.getElementById("selectCategory");
 
 let apiQuotes = [];
+let apiCategory = "no category";
 
 function showLoadingSpinner() {
   loader.hidden = false;
@@ -20,8 +23,14 @@ function removeLoadingSpinner() {
 // Show new quote
 function newQuote() {
   showLoadingSpinner();
+  // Filter quote by category selected
+  let copia = apiQuotes;
+  if (!(apiCategory === "no category")) {
+    copia = apiQuotes.filter((quote) => quote.tag === apiCategory);
+  }
+  console.log(copia);
   // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  const quote = copia[Math.floor(Math.random() * copia.length)];
   // Check if author field is blank and replace it with 'Unknown'
   if (!quote.author) {
     authorText.textContent = "Unknown";
@@ -36,7 +45,25 @@ function newQuote() {
   }
   // Set quote and hide loader
   quoteText.textContent = quote.text;
+  // Set quote category
+  categoryText.textContent = quote.tag;
   removeLoadingSpinner();
+}
+
+// Create categories for selecting quotes
+function createSelectCategoryOptions(categories) {
+  //Add the no category option to select
+  const option = document.createElement("option");
+  option.value = "no category";
+  option.textContent = "no category";
+  selectCategory.appendChild(option);
+  // Add categories for select dynamically
+  categories.forEach((categorie) => {
+    const option = document.createElement("option");
+    option.value = categorie;
+    option.textContent = categorie;
+    selectCategory.appendChild(option);
+  });
 }
 
 // Get quotes from API
@@ -46,6 +73,13 @@ async function getQuotes() {
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
+    const categories = [];
+    apiQuotes.forEach((apiQuote) => {
+      if (!categories.includes(apiQuote.tag)) {
+        categories.push(apiQuote.tag);
+      }
+    });
+    createSelectCategoryOptions(categories);
     newQuote();
   } catch (error) {
     alert("There was a problem with the API");
@@ -61,6 +95,9 @@ function tweetQuote() {
 // Event listeners
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
+selectCategory.addEventListener("change", (e) => {
+  apiCategory = e.target.value;
+});
 
 // On load
 getQuotes();
